8b2e310d4e5c7609178ddb7975a68a27a3dd89b9
Amanda El Rayes <amandaelrayes@gmail.com>
2023-12-04 11:48:46 +0100

    part 2 day 4 done. part 1 is in last commit.

--- day4/Day4.cs
+++ day4/Day4.cs
@@ -7,60 +7,50 @@ namespace day4
     {
         public void Run()
         {
-            using var sr = new StreamReader(@"input.txt");
-            var cardPoints = new List<int>();
-
-
-            while (!sr.EndOfStream)
-            {
-                var line = sr.ReadLine().Split('|');
-                var winningLine = line[0].Split(":")[1];
-                var winningNumbers = winningLine.Split(" ");
-                var mynumbers = line[1].Split(" ");
-
-                var res = winningNumbers.Intersect(mynumbers);
-
-                cardPoints.Add(CalculatePoints(res));
-            }
-            Console.WriteLine(cardPoints.Sum());
-        }
-
-        private int CalculatePoints(IEnumerable<string> res)
-        {
-            var count = 0;
-            var firstSuccess = true;
-            foreach (var item in res)
-            {                
-                var success = int.TryParse(item, out int result);
-                
-                if (success && result != 0 && firstSuccess)
-                {
-                    count = 1;
-                    firstSuccess = false;
-                }
-                else if (success && result != 0)
-                {
-                    count = count * 2;                 
-                }
-            }
-            return count;
-        }
-
-        public static StreamReader ReadFile()
-        {
-            ///////////////HELPLINES///////////////////////
-            // ADD THIS IN RUN
-            //var sr = ReadFile();
-            // THEN YOU CAN DO ONE OF THESE
-            //var input = sr.ReadToEnd();
-            //var line = sr.ReadLine();
-            //while (!sr.EndOfStream)
-            //{
-            //    var line = sr.ReadLine();
-            //}
-            ///////////////////////////////////////////////
             using var sr = new StreamReader("input.txt");
-            return sr;
+            var cardPoints = new List<int>();
+            var matchingNumbers = new Dictionary<int, string>();
+            var copies = new List<int>();
+            //var noOfScratchCards = 0;
+            List<int> noOfScratchCards = new();
+
+            var cards = sr.ReadToEnd().Split("\r\n");
+
+            for (int i = 0; i < cards.Length; i++)
+            {
+                noOfScratchCards = RecursiveMethod(noOfScratchCards, cards, cards[i]);
+            }
+            Console.WriteLine(noOfScratchCards.Count());
+        }
+
+        private static List<int> RecursiveMethod(List<int> noOfScratchCards, string[] cards, string currentCard)
+        {
+
+            noOfScratchCards.Add(1);
+            GetMatchCount(currentCard, out int matchCount, out int currentCardNumber);
+            if (matchCount == 0)
+            {
+                return noOfScratchCards;
+            }
+
+            for (int i = 0; i < matchCount; i++)
+            {
+                RecursiveMethod(noOfScratchCards, cards, cards[currentCardNumber + i]);
+            }
+
+            return noOfScratchCards;
+        }
+
+        public static void GetMatchCount(string card, out int matchCount, out int currentCardNo)
+        {
+            var line = card.Split('|');
+            var winningNumbers = line[0].Split(":")[1].Split(" ");
+            var mynumbers = line[1].Split(" ");
+            var matchingNumbers = winningNumbers.Intersect(mynumbers).Where(x => int.TryParse(x, out int _) == true);
+            matchCount = matchingNumbers.Count();
+            var x = line[0].Split(":")[0];
+            var u = Regex.Match(x, "\\d+").ToString();
+            currentCardNo = int.Parse(u);
         }
     }
 }
