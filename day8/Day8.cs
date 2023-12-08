e86dc17d6fcefec446cd34fbf2f935fb1290aa5d
Amanda El Rayes <amandaelrayes@gmail.com>
2023-12-08 15:07:40 +0100

    day 8 part 2 done. needed hints :/

--- day8/Day8.cs
+++ day8/Day8.cs
@@ -1,36 +1,92 @@
-﻿using System.ComponentModel;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using System.Text.RegularExpressions;
 
 namespace day8
 {
     public class Day8
     {
-        public void Run()
+        public async Task Run()
         {
             var watch = Stopwatch.StartNew();
             using var sr = new StreamReader("input.txt");
             var instructions = sr.ReadLine();
+            Queue<int> direction = CreateQueueOfDirections(ref instructions);
+            sr.ReadLine(); // reading empty line
+            Dictionary<string, List<string>> dict = CreateDictionary(sr);
+
+            Part1(watch, direction, dict);
+            Part2(instructions.Length, direction, dict);
+        }
+
+        private static Queue<int> CreateQueueOfDirections(ref string? instructions)
+        {
             instructions = instructions.Replace('L', '0');
             instructions = instructions.Replace('R', '1');
 
-            var queue = new Queue<int>();
+            var direction = new Queue<int>();
             for (int i = 0; i < instructions.Length; i++)
             {
-                queue.Enqueue(int.Parse(instructions[i].ToString()));
+                direction.Enqueue(int.Parse(instructions[i].ToString()));
             }
-            sr.ReadLine(); // reading empty line
-            Dictionary<string, List<string>> dict = CreateDictionary(sr);
-           
+
+            return direction;
+        }
+
+        private static void Part2(int instructionLength, Queue<int> direction, Dictionary<string, List<string>> dict)
+        {
+            var keys = dict.Keys.Where(x => x.EndsWith('A')).ToList();
+            var runCount = new List<int>();
+            long factor = 1L;
+            foreach (var key in keys)
+            {
+                var count = RunKey(key, direction, dict);
+                factor *= count / instructionLength;
+                runCount.Add(count);
+            }
+
+            // get gcd
+            var gcd = GCD(runCount);
+
+            var result = factor * gcd;
+
+
+            Console.WriteLine("Part 2 Result: " + result);
+        }
+
+        static int GCD(List<int> numbers)
+        {
+            return numbers.Aggregate(GCD);
+        }
+
+        static int GCD(int a, int b)
+        {
+            return b == 0 ? a : GCD(b, a % b);
+        }
+
+        private static int RunKey(string key, Queue<int> direction, Dictionary<string, List<string>> dict)
+        {
+            var count = 0;
+            while (!key.EndsWith('Z'))
+            {
+                int n = direction.Dequeue();
+                key = dict.GetValueOrDefault(key)[n];
+                count++;
+                direction.Enqueue(n);
+            }
+            return count;
+        }
+
+
+        private static void Part1(Stopwatch watch, Queue<int> direction, Dictionary<string, List<string>> dict)
+        {
             var count = 0;
             var key = "AAA";
-
-            while(key != "ZZZ")
+            while (key != "ZZZ")
             {
-                var i = queue.Dequeue();
-                key = dict.GetValueOrDefault(key)[i];
+                int n = direction.Dequeue();
+                key = dict.GetValueOrDefault(key)[n];
                 count++;
-                queue.Enqueue(i);
+                direction.Enqueue(n);
             }
 
             Console.WriteLine($"Steps: {count}, elapsed time {watch.ElapsedMilliseconds} ms ");
@@ -42,9 +98,8 @@ namespace day8
             while (!sr.EndOfStream)
             {
                 var line = sr.ReadLine();
-                var elements = Regex.Matches(line, "[A-Z]{3}");
+                var elements = Regex.Matches(line, "\\w{3}");
                 dict.Add(elements[0].ToString(), new List<string>() { elements[1].ToString(), elements[2].ToString() });
-
             }
             return dict;
         }
--- day8/Program.cs
+++ day8/Program.cs
@@ -1,3 +1,3 @@
 ﻿using day8;
 var program = new Day8();
-program.Run();
+await program.Run();
