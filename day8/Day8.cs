fff95d021f94206ef96e3a9d7cc72c0c07e27f19
Amanda El Rayes <amandaelrayes@gmail.com>
2023-12-08 08:34:19 +0100

    works on testinput. must optimize

--- day8/Day8.cs
+++ day8/Day8.cs
@@ -1,27 +1,64 @@
-﻿namespace day8
+﻿using System.ComponentModel;
+using System.Text.RegularExpressions;
+
+namespace day8
 {
     public class Day8
     {
         public void Run()
         {
-            Console.WriteLine(123);
+            using var sr = new StreamReader("input.txt");
+            var instructions = sr.ReadLine();
+            sr.ReadLine(); // reading empty line
+            Dictionary<string, List<string>> tree = CreateTree(sr);
+
+            var key = "AAA";
+            
+            var count = 0;
+
+            NextNode(instructions, tree, key, ref count, 0);        
+
+            Console.WriteLine(count);
         }
 
-        public static StreamReader ReadFile()
+        private static void NextNode(string? instructions, Dictionary<string, List<string>> tree, string key, ref int count, int position)
         {
-            ///////////////HELPLINES///////////////////////
-            // ADD THIS IN RUN
-            //var sr = ReadFile();
-            // THEN YOU CAN DO ONE OF THESE
-            //var input = sr.ReadToEnd();
-            //var line = sr.ReadLine();
-            //while (!sr.EndOfStream)
-            //{
-            //    var line = sr.ReadLine();
-            //}
-            ///////////////////////////////////////////////
-            using var sr = new StreamReader("input.txt");
-            return sr;
+            var node = tree.GetValueOrDefault(key);
+            if (key == "ZZZ")
+            {
+                Console.WriteLine(count);
+                Environment.Exit(0);
+            }
+            if (position == instructions.Length)
+            {
+                position = 0;
+            }
+            count++;
+            switch (instructions[position])
+            {
+                case 'L':
+                    position += 1;
+                    NextNode(instructions, tree, node.First(), ref count, position);
+                    break;
+                case 'R':
+                    position += 1;
+                    NextNode(instructions, tree, node.Last(), ref count, position);
+                    break;
+            }
+        }
+
+
+        private static Dictionary<string, List<string>> CreateTree(StreamReader sr)
+        {
+            var tree = new Dictionary<string, List<string>>();
+            while (!sr.EndOfStream)
+            {
+                var line = sr.ReadLine();
+                var elements = Regex.Matches(line, "[A-Z]{3}");
+                tree.Add(elements[0].ToString(), new List<string>() { elements[1].ToString(), elements[2].ToString() });
+
+            }
+            return tree;
         }
     }
 }
